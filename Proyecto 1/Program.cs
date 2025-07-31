27d431d9b009bd1d56567cc8e0f4c883a3447519
Víctor_3.1416 <matemorales2020@gmail.com>
2025-07-30 22:24:21 -0600

    Versión sin parser

--- Proyecto 1/Program.cs
+++ Proyecto 1/Program.cs
@@ -1,388 +1,141 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text.RegularExpressions;
 
-public enum TokenType {
-    Variable,
-    Not,
-    And,
-    Or,
-    Imply,
-    Equiv,
-    LParen,
-    RParen
-}
-
-public class Token {
-    public TokenType Type;
-    public string Value;
-
-    public Token(TokenType type, string value = "")
-    {
-        Type = type;
-        Value = value;
-    }
-}
-
-public class Lexer
+namespace CalculadoraTablasDeVerdad
 {
-    private string input;
-    private int position = 0;
-
-    public Lexer(string input)
+    class Programa
     {
-        this.input = input.Replace(" ", "");
-    }
-
-    public List<Token> Tokenize()
-    {
-        List<Token> tokens = new List<Token>();
-
-        while (position < input.Length)
+        static void Main()
         {
-            char current = input[position];
+            Console.Write("Ingrese una proposición compuesta: ");
+            Console.WriteLine("Puede usar caracteres como: →, ->, ↔, <->, ^, &, V, |, ¬, !");
+            string expresionIngresada = Console.ReadLine()!;
 
-            // Ignorar espacios
-            if (char.IsWhiteSpace(current))
+            string expresion = expresionIngresada.Replace(" ", "")
+                                                .Replace("→", "->")
+                                                .Replace("↔", "<->")
+                                                .Replace("∧", "&")
+                                                .Replace("∨", "|")
+                                                .Replace("¬", "!");
+
+            var variables = ObtenerVariables(expresion);
+            var subExpresiones = ObtenerSubExpresiones(expresion);
+
+            int total = 1 << variables.Count;
+            Console.WriteLine();
+
+            foreach (var caracter in variables)
+                Console.Write(caracter + "\t");
+            foreach (var sub in subExpresiones)
+                Console.Write(sub + "\t");
+            Console.WriteLine();
+
+            for (int i = 0; i < total; i++)
             {
-                position++;
-                continue;
-            }
+                var asignacion = new Dictionary<char, bool>();
+                for (int j = 0; j < variables.Count; j++)
+                    asignacion[variables[j]] = ((i >> (variables.Count - j - 1)) & 1) == 1;
 
-            // Operadores de dos o más caracteres
-            if (position + 2 < input.Length && input.Substring(position, 3) == "<->")
-            {
-                tokens.Add(new Token(TokenType.Equiv));
-                position += 3;
-                continue;
-            }
-            if (position + 1 < input.Length && input.Substring(position, 2) == "->")
-            {
-                tokens.Add(new Token(TokenType.Imply));
-                position += 2;
-                continue;
-            }
+                foreach (var v in variables)
+                    Console.Write((asignacion[v] ? "V" : "F") + "\t");
 
-            switch (current)
-            {
-                case '¬':
-                case '!':
-                case '-':
-                case '~':
-                    tokens.Add(new Token(TokenType.Not));
-                    position++;
-                    break;
-                case '∧':
-                case '^':
-                case '&':
-                    tokens.Add(new Token(TokenType.And));
-                    position++;
-                    break;
-                case '∨':
-                case 'v':
-                case '|':
-                    tokens.Add(new Token(TokenType.Or));
-                    position++;
-                    break;
-                case '→':
-                    tokens.Add(new Token(TokenType.Imply));
-                    position++;
-                    break;
-                case '↔':
-                    tokens.Add(new Token(TokenType.Equiv));
-                    position++;
-                    break;
-                case '(':
-                    tokens.Add(new Token(TokenType.LParen));
-                    position++;
-                    break;
-                case ')':
-                    tokens.Add(new Token(TokenType.RParen));
-                    position++;
-                    break;
-                default:
-                    // Variables: letras y números (p, q1, r2, etc.)
-                    if (char.IsLetter(current))
-                    {
-                        int start = position;
-                        position++;
-                        while (position < input.Length && (char.IsLetterOrDigit(input[position]) || input[position] == '_'))
-                            position++;
-                        string varName = input.Substring(start, position - start);
-                        tokens.Add(new Token(TokenType.Variable, varName));
-                    }
-                    else
-                    {
-                        throw new Exception($"Carácter inválido: {current}");
-                    }
-                    break;
-            }
-        }
-
-        return tokens;
-    }
-}
-
-public abstract class Expr
-{
-    public abstract bool Evaluate(Dictionary<string, bool> vars);
-    public abstract List<string> GetVariables();
-}
-
-public class VariableExpr : Expr
-{
-    public string Name;
-
-    public VariableExpr(string name)
-    {
-        Name = name;
-    }
-
-    public override bool Evaluate(Dictionary<string, bool> vars)
-    {
-        return vars[Name];
-    }
-
-    public override List<string> GetVariables()
-    {
-        return new List<string> { Name };
-    }
-}
-
-public class NotExpr : Expr
-{
-    public Expr Operand;
-
-    public NotExpr(Expr operand)
-    {
-        Operand = operand;
-    }
-
-    public override bool Evaluate(Dictionary<string, bool> vars)
-    {
-        return !Operand.Evaluate(vars);
-    }
-
-    public override List<string> GetVariables()
-    {
-        return Operand.GetVariables();
-    }
-}
-
-public enum BinaryOp
-{
-    And, Or, Imply, Equiv
-}
-
-public class BinaryExpr : Expr
-{
-    public Expr Left;
-    public Expr Right;
-    public BinaryOp Op;
-
-    public BinaryExpr(Expr left, Expr right, BinaryOp op)
-    {
-        Left = left;
-        Right = right;
-        Op = op;
-    }
-
-    public override bool Evaluate(Dictionary<string, bool> vars)
-    {
-        bool a = Left.Evaluate(vars);
-        bool b = Right.Evaluate(vars);
-
-        return Op switch
-        {
-            BinaryOp.And => a && b,
-            BinaryOp.Or => a || b,
-            BinaryOp.Imply => !a || b,
-            BinaryOp.Equiv => a == b,
-            _ => throw new Exception("Operador desconocido")
-        };
-    }
-
-    public override List<string> GetVariables()
-    {
-        return Left.GetVariables().Union(Right.GetVariables()).ToList();
-    }
-}
-
-public class Parser
-{
-    private List<Token> tokens;
-    private int position = 0;
-
-    public Parser(List<Token> tokens)
-    {
-        this.tokens = tokens;
-    }
-
-    public Expr Parse()
-    {
-        return ParseEquiv();
-    }
-
-    private Expr ParseEquiv()
-    {
-        Expr left = ParseImply();
-        while (Match(TokenType.Equiv))
-        {
-            Expr right = ParseImply();
-            left = new BinaryExpr(left, right, BinaryOp.Equiv);
-        }
-        return left;
-    }
-
-    private Expr ParseImply()
-    {
-        Expr left = ParseOr();
-        while (Match(TokenType.Imply))
-        {
-            Expr right = ParseOr();
-            left = new BinaryExpr(left, right, BinaryOp.Imply);
-        }
-        return left;
-    }
-
-    private Expr ParseOr()
-    {
-        Expr left = ParseAnd();
-        while (Match(TokenType.Or))
-        {
-            Expr right = ParseAnd();
-            left = new BinaryExpr(left, right, BinaryOp.Or);
-        }
-        return left;
-    }
-
-    private Expr ParseAnd()
-    {
-        Expr left = ParseNot();
-        while (Match(TokenType.And))
-        {
-            Expr right = ParseNot();
-            left = new BinaryExpr(left, right, BinaryOp.And);
-        }
-        return left;
-    }
-
-    private Expr ParseNot()
-    {
-        if (Match(TokenType.Not))
-        {
-            return new NotExpr(ParseNot());
-        }
-        return ParsePrimary();
-    }
-
-    private Expr ParsePrimary()
-    {
-        if (Match(TokenType.LParen))
-        {
-            Expr expr = ParseEquiv();
-            Expect(TokenType.RParen);
-            return expr;
-        }
-
-        if (Match(TokenType.Variable, out Token token))
-        {
-            return new VariableExpr(token.Value);
-        }
-
-        throw new Exception("Expresión inválida");
-    }
-
-    private bool Match(TokenType type)
-    {
-        if (position < tokens.Count && tokens[position].Type == type)
-        {
-            position++;
-            return true;
-        }
-        return false;
-    }
-
-    private bool Match(TokenType type, out Token token)
-    {
-        if (position < tokens.Count && tokens[position].Type == type)
-        {
-            token = tokens[position];
-            position++;
-            return true;
-        }
-        token = null!;
-        return false;
-    }
-
-    private void Expect(TokenType type)
-    {
-        if (!Match(type))
-        {
-            throw new Exception($"Se esperaba: {type}");
-        }
-    }
-}
-
-public static class TruthTableGenerator
-{
-    public static void Generate(Expr expr)
-    {
-        var variables = expr.GetVariables().Distinct().ToList();
-        int rows = 1 << variables.Count;
-
-        Console.WriteLine(string.Join(" ", variables) + " | Resultado");
-
-        for (int i = 0; i < rows; i++)
-        {
-            var values = new Dictionary<string, bool>();
-            for (int j = 0; j < variables.Count; j++)
-            {
-                bool val = (i & (1 << (variables.Count - j - 1))) != 0;
-                values[variables[j]] = val;
-            }
-
-            bool result = expr.Evaluate(values);
-            string line = string.Join(" ", variables.Select(v => values[v] ? "V" : "F"));
-            Console.WriteLine($"{line} | {(result ? "V" : "F")}");
-        }
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        while (true)
-        {
-            Console.WriteLine("*-*-*-*-* CALCULADORA DE PROPOSICIONES *-*-*-*-*");
-            Console.WriteLine("Caracteres válidos: ^, v, ->, <->, ¬, a,b,c...z");
-            Console.WriteLine("¿Desea ingresar una nueva proposición? (s/n): ");
-            string operar = Console.ReadLine()!.ToUpper();
-            if (operar == "S")
-            {
-                Console.WriteLine("Ingrese la expresión lógica:");
-                string input = Console.ReadLine()!;
-
-                try
+                var valoresSub = new Dictionary<string, bool>();
+                foreach (var sub in subExpresiones)
                 {
-                    var lexer = new Lexer(input);
-                    var tokens = lexer.Tokenize();
-
-                    var parser = new Parser(tokens);
-                    var expr = parser.Parse();
-
-                    TruthTableGenerator.Generate(expr);
+                    string evaluada = ReemplazarVariables(sub, asignacion);
+                    bool resultado = EvaluarExpresion(evaluada);
+                    valoresSub[sub] = resultado;
+                    Console.Write((resultado ? "V" : "F") + "\t");
                 }
-                catch (Exception ex)
+
+                Console.WriteLine();
+            }
+        }
+
+        static List<char> ObtenerVariables(string expresion)
+        {
+            HashSet<char> letras = new HashSet<char>();
+            foreach (char c in expresion)
+                if (char.IsLetter(c))
+                    letras.Add(c);
+            var lista = new List<char>(letras);
+            lista.Sort();
+            return lista;
+        }
+
+        static List<string> ObtenerSubExpresiones(string expr)
+        {
+            List<string> subs = new List<string>();
+            Stack<int> pila = new Stack<int>();
+            for (int i = 0; i < expr.Length; i++)
+            {
+                if (expr[i] == '(')
+                    pila.Push(i);
+                else if (expr[i] == ')')
                 {
-                    Console.WriteLine($"Error: {ex.Message}");
+                    int ini = pila.Pop();
+                    string sub = expr.Substring(ini, i - ini + 1);
+                    if (!subs.Contains(sub))
+                        subs.Add(sub);
                 }
             }
-            else
+            if (!subs.Contains(expr))
+                subs.Add(expr);
+            return subs;
+        }
+
+        static string ReemplazarVariables(string expr, Dictionary<char, bool> valores)
+        {
+            string resultado = expr;
+            foreach (var kvp in valores)
+                resultado = resultado.Replace(kvp.Key.ToString(), kvp.Value ? "1" : "0");
+            return resultado;
+        }
+
+        static bool EvaluarExpresion(string expr)
+        {
+            while (expr.Contains("("))
             {
-                break;
+                int inicio = expr.LastIndexOf('(');
+                int fin = expr.IndexOf(')', inicio);
+                string sub = expr.Substring(inicio + 1, fin - inicio - 1);
+                bool valor = Evaluar(sub);
+                expr = expr.Substring(0, inicio) + (valor ? "1" : "0") + expr.Substring(fin + 1);
             }
+            return Evaluar(expr);
+        }
+
+        static bool Evaluar(string expr)
+        {
+            expr = Regex.Replace(expr, @"!\s*1", "0");
+            expr = Regex.Replace(expr, @"!\s*0", "1");
+
+            while (Regex.IsMatch(expr, @"(1|0)&(1|0)"))
+                expr = Regex.Replace(expr, @"(1|0)&(1|0)", m =>
+                {
+                    return (m.Groups[1].Value == "1" && m.Groups[2].Value == "1") ? "1" : "0";
+                });
+
+            while (Regex.IsMatch(expr, @"(1|0)\|(1|0)"))
+                expr = Regex.Replace(expr, @"(1|0)\|(1|0)", m =>
+                {
+                    return (m.Groups[1].Value == "1" || m.Groups[2].Value == "1") ? "1" : "0";
+                });
+
+            while (Regex.IsMatch(expr, @"(1|0)->(1|0)"))
+                expr = Regex.Replace(expr, @"(1|0)->(1|0)", m =>
+                {
+                    return (m.Groups[1].Value == "1" && m.Groups[2].Value == "0") ? "0" : "1";
+                });
+
+            while (Regex.IsMatch(expr, @"(1|0)<->(1|0)"))
+                expr = Regex.Replace(expr, @"(1|0)<->(1|0)", m =>
+                {
+                    return (m.Groups[1].Value == m.Groups[2].Value) ? "1" : "0";
+                });
+
+            return expr.Trim() == "1";
         }
     }
 }
--- Proyecto 1/obj/Debug/net9.0/Proyecto 1.AssemblyInfo.cs
+++ Proyecto 1/obj/Debug/net9.0/Proyecto 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a94309929370d23af4e99b38ab97326cc905716")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
